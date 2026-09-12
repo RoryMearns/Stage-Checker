51a9b64ba05e29fb5be9a3ac4e0a2412ed19aaa0
Rory <dev.r.mearns@gmail.com>
2026-09-13 07:48:46 +1200

    network first approach when online

--- sw.js
+++ sw.js
@@ -89,16 +89,12 @@ self.addEventListener('fetch', event => {
     }
 
     event.respondWith(
-        caches.match(event.request).then(cached => {
-            if (cached) return cached;
-
-            return fetch(event.request).then(response => {
-                if (response.ok && (response.type === 'basic' || response.type === 'cors')) {
-                    const responseClone = response.clone();
-                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
-                }
-                return response;
-            }).catch(() => cached);
-        })
+        fetch(event.request).then(response => {
+            if (response.ok && (response.type === 'basic' || response.type === 'cors')) {
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+            }
+            return response;
+        }).catch(() => caches.match(event.request))
     );
 });
